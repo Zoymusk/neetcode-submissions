6e7ab21df45418cc330aa623fa23598d58ff7a14
Zoya Muskaan <zoyamuskaan20@gmail.com>
2026-09-25 21:18:22 +0530

    Add: brick-wall - submission-0

---
+++ Data Structures & Algorithms/brick-wall/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int leastBricks(List<List<Integer>> wall) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int maxGap = 0;
+        for (List<Integer> row : wall) {
+            int sum = 0;
+            for (int i = 0; i < row.size() - 1; i++) {
+                sum += row.get(i);
+                map.put(sum, map.getOrDefault(sum, 0) + 1);
+                maxGap = Math.max(maxGap, map.get(sum));
+            }
+        }
+        return wall.size() - maxGap;
+    }
+}
